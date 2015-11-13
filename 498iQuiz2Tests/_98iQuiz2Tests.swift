c24b2cadd1163d51df48909f922490c0bda61957
Rachel Kipps <rachelkipps@gmail.com>
2015-11-12 18:45:26 -0800

    Initial Commit

---
+++ 498iQuiz2Tests/_98iQuiz2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _98iQuiz2Tests.swift
+//  498iQuiz2Tests
+//
+//  Created by Rachel Kipps on 11/12/15.
+//  Copyright © 2015 Rachel Kipps. All rights reserved.
+//
+
+import XCTest
+@testable import _98iQuiz2
+
+class _98iQuiz2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
